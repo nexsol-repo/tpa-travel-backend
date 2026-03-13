e233c4158f7a14220ea2c75245b79f6f3a3982e0
devback90 <devback90.nexsol@gmail.com>
2026-03-13 09:14:56 +0900

    exception refactoring

--- core/core-api/src/main/java/com/nexsol/tpa/core/support/error/CoreApiErrorCode.java
+++ core/core-api/src/main/java/com/nexsol/tpa/core/support/error/CoreApiErrorCode.java
@@ -3,6 +3,26 @@ package com.nexsol.tpa.core.support.error;
 public enum CoreApiErrorCode {
 
     // 기본
-    T500, T400, T401,
+    T500,
+    T400,
+    T401,
 
+    // 견적
+    T1000,
+    T1001,
+    T1002,
+
+    // 참조데이터
+    T2000,
+    T2001,
+
+    // 계약
+    T3000,
+    T3001,
+    T3002,
+
+    // 본인인증
+    T5000,
+    T5001,
+    T5002,
 }
